0721ec2bea9a815989b56f0d9e775975d7aecef1
Tkazer <1319923129@qq.com>
2023-03-26 11:36:02 +0800

    Update 3/26

---
+++ OS-ImGui/OS-ImGui.h
@@ -0,0 +1,109 @@
+#include "OS-ImGui_Struct.h"
+#include <iostream>
+#include <string>
+#include <functional>
+#include <codecvt>
+#include <vector>
+
+/****************************************************
+* Copyright (C)	: Liv
+* @file			: OS-ImGui.h
+* @author		: Liv
+* @email		: 1319923129@qq.com
+* @version		: 1.0
+* @date			: 2023/3/26	11:32
+****************************************************/
+
+namespace OSImGui
+{
+	class D3DDevice
+	{
+	public:
+		ID3D11Device* g_pd3dDevice = nullptr;
+		ID3D11DeviceContext* g_pd3dDeviceContext = nullptr;
+		IDXGISwapChain* g_pSwapChain = nullptr;
+		ID3D11RenderTargetView* g_mainRenderTargetView = nullptr;
+		bool CreateDeviceD3D(HWND hWnd);
+		void CleanupDeviceD3D();
+		void CreateRenderTarget();
+		void CleanupRenderTarget();
+	};
+
+	static D3DDevice Device;
+
+	enum WindowType
+	{
+		NEW,
+		ATTACH
+	};
+
+	class WindowData
+	{
+	public:
+		HWND  hWnd = NULL;
+		HINSTANCE hInstance;
+		std::string Name;
+		std::string ClassName;
+		Vec2 Pos;
+		Vec2 Size;
+		ImColor BgColor{ 255, 255, 255 };
+	};
+
+	class OSImGui_Base
+	{
+	private:
+		// 回调函数
+		std::function<void()> CallBackFn = nullptr;
+		// 退出标识
+		bool EndFlag = false;
+		// 启动类型
+		WindowType Type = NEW;
+	protected:
+		// 窗口数据
+		WindowData Window;
+		// 目标窗口数据
+		WindowData DestWindow;
+	public:
+		// 创建一个新窗口
+		void NewWindow(std::string WindowName, Vec2 WindowSize, std::function<void()> CallBack);
+		// 附加到另一个窗口上
+		void AttachAnotherWindow(std::string DestWindowName, std::string DestWindowClassName, std::function<void()> CallBack);
+		// 退出
+		void Quit();
+	private:
+		void MainLoop();
+		bool CreateMyWindow();
+		bool UpdateWindowData();
+		bool InitImGui();
+		void CleanImGui();
+		bool PeekEndMessage();
+		std::wstring StringToWstring(std::string& str);
+	};
+
+	class OSImGui : public OSImGui_Base, public Singleton<OSImGui>
+	{
+	public:
+		// 文本
+		void Text(std::string Text, Vec2 Pos, ImColor Color);
+		// 描边文本
+		void StrokeText(std::string Text, Vec2 Pos, ImColor Color);
+		// 矩形
+		void Rectangle(Vec2 Pos, Vec2 Size, ImColor Color, float Thickness, float Rounding = 0);
+		void RectangleFilled(Vec2 Pos, Vec2 Size, ImColor Color, float Rounding = 0);
+		// 线
+		void Line(Vec2 From, Vec2 To, ImColor Color, float Thickness);
+		// 圆形
+		void Circle(Vec2 Center, float Radius, ImColor Color,float Thickness, int Num = 50);
+		void CircleFilled(Vec2 Center, float Radius, ImColor Color, int Num = 50);
+		// 圆弧
+		void Arc(Vec2 Pos, float Radius, ImColor Color, float Angel, float Proportion, float Thickness);
+		// 连接点
+		void ConnectPoints(std::vector<Vec2> Points, ImColor Color, float Thickness);
+		// 勾选框
+		void MyCheckBox(const char* str_id, bool* v);
+		void MyCheckBox2(const char* str_id, bool* v);
+		void MyCheckBox3(const char* str_id, bool* v);
+		void MyCheckBox4(const char* str_id, bool* v);
+	};
+}
+
---
+++ OS-ImGui/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "OS-ImGui.h"
+
+void DrawCallBack()
+{
+	ImGui::Begin("Menu");
+	{
+		ImGui::Text("This is a text.");
+		if (ImGui::Button("Button"))
+		{
+			//...
+		}
+		static bool a = false, b = false, c = false, d = false;
+		OSImGui::OSImGui::get().MyCheckBox("CheckBox1", &a);
+		OSImGui::OSImGui::get().MyCheckBox2("CheckBox2", &b);
+		OSImGui::OSImGui::get().MyCheckBox3("CheckBox3", &c);
+		OSImGui::OSImGui::get().MyCheckBox4("CheckBox4", &d);
+	}ImGui::End();
+
+	//...
+}
+
+int main()
+{
+	OSImGui::OSImGui::get().NewWindow("WindowName", Vec2(500, 500), DrawCallBack);
+	//OSImGui::OSImGui::get().AttachAnotherWindow("Title","", Draw);
+	system("pause");
+	return 0;
+}
